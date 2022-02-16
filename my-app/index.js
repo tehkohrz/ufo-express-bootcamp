d63989656248c6db1900f45c3a812d3feb4db25d
tehkohrz <dongkunkoh@hotmail.com>
2022-02-16 21:28:33 +0800

    Completed up to delete, recloning for css

--- my-app/index.js
+++ my-app/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import methodOverride from 'method-override';
 
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+
 import jsStore from './jsonFileStorage.js';
 
 // File paths
@@ -12,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
 
 // NO EXTENSION
 // List of all the sighitngs
@@ -49,6 +52,46 @@ app.get('/sighting/:index', (request, response) => {
     response.render('sighting-entry', { sightingEntry });
   }
   jsStore.read('data.json', readhandler);
+});
+
+// Edit the data for a sighting
+app.get('/sighting/:index/edit', (request, response) => {
+  function readhandler(error, data) {
+    const { index } = request.params;
+    const sightingEntry = { ...data.sightings[index] };
+    sightingEntry.keys = Object.keys(sightingEntry);
+    sightingEntry.indexNo = index;
+    response.render('sighting-edit', { sightingEntry });
+  }
+  jsStore.read('data.json', readhandler);
+});
+
+// PUT request for the overwrite of the data.json
+// Redirect to sighting/:index to display data
+app.put('/sighting/:index/edit', (request, response) => {
+  const { index } = request.params;
+  function readhandler(error, data) {
+    const sightingEntry = request.body;
+    data.sightings[index] = request.body;
+    jsStore.write('data.json', data, (err) => {
+      console.log('Write sucess!');
+    });
+    response.render('sightings-entry', { sightingEntry });
+  }
+  jsStore.read('data.json', readhandler);
+});
+
+// Delete Entry
+app.delete(('/sighting/:index'), (request, response) => {
+  function readhander(error, data) {
+    const { index } = request.params;
+    data.sightings.splice(index, 1);
+    const noOfSightings = data.sightings.length;
+    jsStore.write('data.json', data, (err) => { console.log('Deleted entry');
+    });
+    response.render('main', { noOfSightings });
+  }
+  jsStore.read('data.json', readhander);
 });
 
 app.listen(3004);
